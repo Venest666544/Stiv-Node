f497611464f4dc4cd6f11fb66ffced5170a82a92
Stiven <guevarastiven08@gmail.com>
2026-03-25 08:32:01 -0500

    Configuración de DB dinámica para deploy

--- db.js
+++ db.js
@@ -1,14 +1,20 @@
 const mysql = require('mysql2');
 
+// Usamos variables de entorno (process.env) para la nube
+// Si no existen usará los valores por defecto tras el "||"
 const conexion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'notas_Db'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'notas_Db',
+    port: process.env.DB_PORT || 3306
 });
 
 conexion.connect((err) => {
-    if (err) throw err;
+    if (err) {
+        console.error("❌ Error de conexión:", err.message);
+        return;
+    }
     console.log("✅ Conectado a la Base de Datos MySQL");
 });
 
