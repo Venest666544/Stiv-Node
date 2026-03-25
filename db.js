beac2c97f5d1634e5970047dcb59ac0aab788c19
Stiven <guevarastiven08@gmail.com>
2026-03-25 10:21:56 -0500

    CAMBIOS DE MÁS EN DB.JS

--- db.js
+++ db.js
@@ -1,5 +1,12 @@
 const mysql = require('mysql2');
 
+console.log("🔍 DB CONFIG:", {
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
+});
+
 const pool = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
