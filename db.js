a6ce44d0c241cbd34666e2aa2ce4ccba85b558bd
Stiven <guevarastiven08@gmail.com>
2026-03-25 09:39:09 -0500

    Fix: Nombres de variables MYSQL y separacion de host/puerto

--- db.js
+++ db.js
@@ -1,16 +1,15 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
-    ssl: { rejectUnauthorized: false }, // Obligatorio para Railway
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306, // Intentará usar 36933 de Vercel
+    ssl: { rejectUnauthorized: false },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-// Exportamos el pool con soporte de promesas
 module.exports = pool.promise();
