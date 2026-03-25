1db22988a9787c50796021b7055524ecd614229e
Stiven <guevarastiven08@gmail.com>
2026-03-25 08:46:30 -0500

    Solución final: nombres de DB corregidos y puerto dinámico

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const conexion = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'notas_Db',
+    database: process.env.DB_NAME || 'notas_db',
     port: process.env.DB_PORT || 3306
 });
 
